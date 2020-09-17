c05523a233966fc31be7622a8a3cc2d4a0da62cb
Aigul Novikova <aigulakagimaeva@mail.ru>
2020-09-17 21:41:07 +0300

    добавить автозаполнение инпута и исправить код

--- script/index.js
+++ script/index.js
@@ -1,54 +1,33 @@
 const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
+const popupSaveButton = document.querySelector('.popup__save-button');
+const nameInput = document.querySelector('.input_edit-name');
+const jobInput = document.querySelector('.input_edit-job');
+const userName = document.querySelector('.profile__name');
+const userJob = document.querySelector('.profile__job');
 function addPopupEditForm() {
     popup.classList.add('popup_edit-form');
+    nameInput.value = userName.textContent;
+    jobInput.value = userJob.textContent;
 }
 
 const popupCloseButton = popup.querySelector('.popup__close-button');
 function deletePopupEditForm() {
     popup.classList.remove('popup_edit-form');
-    form.name.value = '';
-}
-let form = document.forms.new;
-function addCustomUserCard(evt) {
-
-		let name = form.name.value;
-		//создаем елемент-контейнер
-		createCard(name);
-		//запрещаем перезагрузку страницы
-		evt.preventDefault();
-		//закрываем попап при событии submit
-        deleteClassToPopup();
-        addDisabledForButton();
 }
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__form-container');
-function formSubmitHandler (event) {
-    event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+const formElement = document.querySelector('.popup__form-container');
 
-
-    // Находим поля формы в DOM
-    let nameInput = document.querySelector('.popup_edit-name');
-    let jobInput = document.querySelector('.popup_edit-job');
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     
-    // Получите значение полей из свойства value
-    nameInput = nameInput.value;
-    jobInput = jobInput.value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let userName = document.querySelector('.profile__name');
-    let userJob = document.querySelector('.profile__job');
-
     // Вставьте новые значения с помощью textContent
-    userName.textContent = nameInput;
-    userJob.textContent = jobInput;
+    userName.textContent = nameInput.value;
+    userJob.textContent = jobInput.value;
 
-    deleteClassToPopup();
-    addDisabledForButton();
 }
-const popupSaveButton = document.querySelector('.popup__save-button');
 
 
 // Прикрепляем обработчик к форме:
